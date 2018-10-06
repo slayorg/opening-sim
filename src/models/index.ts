e6c1310759713edded0d78324cd40cc73c05f0c8
Tylian <immatyger@gmail.com>
2018-10-06 18:52:54 -0400

    started work on skills

--- src/models/index.ts
+++ src/models/index.ts
@@ -8,13 +8,29 @@ export interface ISkillModifier{
 	effects: Array<ISkillProperty>;
 }
 
+// TODO expand this to in game types
+export enum DamageAspect {
+	Blunt = "blunt",
+	Piercing = "piercing",
+	Slashing = "slashing",
+	Unaspected = "unaspected",
+}
+
 export interface ISkill {
 	id: number;
 	name: string;
 	type: string;
 	potency?: number;
+	cast?: number;
 	recast?: number;
-	gcd?: boolean;
+	group?: number;
+	aspect?: DamageAspect;
+	icon?: number;
+	// TODO make a proper type out of this? in game it's two values, cost type and cost valu
+	// the cost type covers hp, mp, tp, hp percentage, 'all mp', plus guage costs, like tornado kick
+	cost?: { 									
+		[key: string]: number;
+	},
 	requirements?: Array<ISkillProperty>;
 	modifiers?: Array<ISkillModifier>;
 	after?: Array<any>;
@@ -34,11 +50,20 @@ export interface IPropertyItem{
 export interface IStatusEffect{
 	id: string;
 	value: number;
+	start: number;
+	duration: number;
 }
 
 export interface ICombatState{
 	statuses: Array<IStatusEffect>;
-	debuffs: Array<any>;
+	action: string;
+	success: boolean; // Action completed successfully (e.g. combo prerequisites met)
+	gauge: {
+		[key: string]: any;
+	},
+	recast: {
+		[key: number]: number;
+	},
 	damage: number;
 	time: number;
 }
--- src/services/skillService.ts
+++ src/services/skillService.ts
@@ -13,15 +13,46 @@ const skillArray: Array<ISkill> = [];
 export {skillMap, skillArray};
 
 export function processSkill(params: {[key: string]: number|boolean}, state: ICombatState, skill: ISkill): ICombatState {
-	return null;
+	// Update time, advance time to the next period you can use a skill
+	// TODO handle casting
+	state.time += Math.max(0.85, state.recast[skill.group]);
+	state.recast[skill.group] = state.time + skill.recast / 10;
+
+	state.action = skill.name;
+	
+	return state;
 }
 
+// TODO pls fix this matt
+function getDefaultGauge(job: number): {[key: string]: number} {
+	switch(job) {
+		case 20: // MNK
+			return {
+				chakra: 0,
+				lightningStacks: 0,
+				lightningTime: 0
+			}
+	}
+	return {}
+}
 
-export function processSkills(params: {[key: string]: number|boolean}, skills: Array<ISkill>): void {
+export function processSkills(params: {[key: string]: number|boolean}, skills: Array<ISkill>): Array<ICombatState> {
 	let state: ICombatState = {
 		statuses: [],
-		debuffs: [],
 		damage: 0,
-		time: 0
+		time: 0,
+		action: null,
+		gauge: getDefaultGauge(20),
+		success: false,
+		recast: {}
 	};
+
+	let states: Array<ICombatState> = [];
+
+	skills.forEach(skill => {
+		state = processSkill(params, JSON.parse(JSON.stringify(state)), skill);
+		states.push(state);
+	});
+
+	return states;
 }
